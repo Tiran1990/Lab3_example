a328f9d497219364d04260e26c5a061237f0f195
Tiran1990 <t.hesawi1990@gmail.com>
2021-10-23 14:36:33 +0300

    2021 add

--- exmp/src/exmp/ex.java
+++ exmp/src/exmp/ex.java
@@ -3,7 +3,7 @@ package exmp;
 public class ex {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World 2022!!!");
+		System.out.println("Hello World 2021-2022!!!");
 	}
 
 }
