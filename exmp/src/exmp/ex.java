6e48a2976a196ddc25f4d36ae89055cbb3e17296
Tiran1990 <t.hesawi1990@gmail.com>
2021-10-23 15:11:32 +0300

    i=0 because work with array

--- exmp/src/exmp/ex.java
+++ exmp/src/exmp/ex.java
@@ -4,9 +4,9 @@ public class ex {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World 2021-2022!!!");
-		int i=0; // loop use   
-		for( i=1;i<10;i++);
-		
+		int i=0; // loop use
+		for( i=0;i<10;i++);// loop
+
 	}
 
 }
